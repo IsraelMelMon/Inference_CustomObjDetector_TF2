fdfdee8ba2913916251de3b10a1dc095c7827300
IsraelMelMon <israelmelendezmontoya@gmail.com>
2021-02-09 03:31:09 -0600

    stable

--- infer.py
+++ infer.py
@@ -232,7 +232,7 @@ if __name__=="__main__":
     parser.add_argument(
         "--labels_path",
         help="Labels map path that indicates different classes" ,
-        default="/Users/israel/Inf_ObjDet_TF2HUB/cool-label_map.pbtxt")
+        default="label_map.pbtxt")
 
     args = parser.parse_args()
 
